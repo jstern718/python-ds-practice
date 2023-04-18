a02f317d321deead0780b1cdb3e057844f594325
Jonathan Stern <jstern718@gmail.com>
2023-04-18 14:31:49 -0400

    initial commit

---
+++ 10_frequency.py
@@ -0,0 +1,15 @@
+def frequency(lst, search_term):
+    """Return frequency of term in lst.
+
+        >>> frequency([1, 4, 3, 4, 4], 4)
+        3
+
+        >>> frequency([1, 4, 3], 7)
+        0
+    """
+
+    freq = lst.count(search_term)
+    return freq
+
+print(frequency([1,4,3,4,4], 4))
+print(frequency([1,4,3], 7))
